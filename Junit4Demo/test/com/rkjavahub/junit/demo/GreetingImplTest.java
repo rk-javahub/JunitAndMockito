83cf8e2265e1a8a45511f322e5d6724464a4f0c2
ROHIT KUMBHAR <kumbharrohit13@gmail.com>
2023-11-23 13:34:43 +0530

    Change in project name

--- Junit4Demo/test/com/rkjavahub/junit/demo/GreetingImplTest.java
+++ Junit4Demo/test/com/rkjavahub/junit/demo/GreetingImplTest.java
@@ -2,14 +2,24 @@ package com.rkjavahub.junit.demo;
 
 import static org.junit.Assert.*;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class GreetingImplTest {
 
+	private Greeting greeting;
+
+	@Before
+	public void setup() {
+		System.out.println("Inside initial setup");
+		greeting = new GreetingImpl();
+	}
+
 	@Test
 	public void greetShouldReturnValidOutput() {
-		Greeting greetingImplTest = new GreetingImpl();
-		String message = greetingImplTest.greet("Junit");
+		System.out.println("Inside greetShouldReturnValidOutput testcase");
+		String message = greeting.greet("Junit");
 
 		// Checking for null
 		assertNotNull(message);
@@ -20,14 +30,22 @@ public class GreetingImplTest {
 
 	@Test(expected = IllegalArgumentException.class)
 	public void greetShouldThrowIllegalArgumentExceptionFor_NameIsNull() {
+		System.out.println("Inside greetShouldThrowIllegalArgumentExceptionFor_NameIsNull testcase");
 		Greeting greetingImplTest = new GreetingImpl();
 		greetingImplTest.greet(null);
 	}
 
 	@Test(expected = IllegalArgumentException.class)
 	public void greetShouldThrowIllegalArgumentExceptionFor_NameIsEmpty() {
+		System.out.println("Inside greetShouldThrowIllegalArgumentExceptionFor_NameIsEmpty testcase");
 		Greeting greetingImplTest = new GreetingImpl();
 		greetingImplTest.greet("");
+	}
+
+	@After
+	public void tearDownProcess() {
+		System.out.println("Inside teardown method");
+		greeting = null;
 	}
 
 }
